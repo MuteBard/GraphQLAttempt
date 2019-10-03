55ba4361a41c6800b0bf2e7647ec9bb52f325fed
MuteBard <csevere@brandeis.edu>
2019-10-02 23:25:48 -0400

    need schema for GraphQL

--- Backend/readMe.md
+++ Backend/readMe.md
@@ -1,2 +1,6 @@
 # Mini Project's Layout
 ![](plan.png)
+
+### Step 1
+![](gql1.png)
+![](gql2.png)
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,14 @@
 const express = require("express")
+//glue layer between graphql and express
+const expressGraphQL = require("express-graphql")
 const app = express()
+
+//any request that comes into our app, looking for the route, /GraphQL, we want the GraphQL library to handle it
+app.use('/graphql', expressGraphQL({
+    graphiql : true
+}))
 
 app.listen(4000, () => {
     console.log("connected on port 4000")
 })
+
